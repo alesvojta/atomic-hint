69cb409d5dc8f471203faeb5eb23575d3102b722
Aleš Vojta <al3s.vojta@gmail.com>
2020-04-18 17:21:07 +0200

    chore: update libraries

--- src/shared/questions.ts
+++ src/shared/questions.ts
@@ -11,7 +11,7 @@ export const wrapperElements: TWrapperElements = {
     a: { o: 0, m: 2, a: 3 },
     button: { o: 0, m: 1, a: 4 },
     div: { o: 0, m: 1, a: 4 },
-    other: { o: 0, m: 0, a: 0 }
+    other: { o: 0, m: 0, a: 0 },
 }
 
 const wrapper: TWrapper = (index: string) => wrapperElements[index]
@@ -27,64 +27,64 @@ export const questionsMap: TQuestionMap = {
     hasMolecules: result({ o: 4, m: 1, a: 0 }, { o: 0, m: 2, a: 3 }),
     embedInMolecule: result({ o: 0, m: 1, a: 4 }, { o: 3, m: 2, a: 0 }),
     embedInOrganism: result({ o: 0, m: 3, a: 2 }, { o: 3, m: 1, a: 1 }),
-    wrapper
+    wrapper,
 }
 
 export const questions = [
     {
         type: 'confirm',
         name: 'alone',
-        message: 'Will the component only ever be used once per page?'
+        message: 'Will the component only ever be used once per page?',
     },
     {
         type: 'confirm',
         name: 'contexts',
-        message: 'Do you expect to use the component in multiple different contexts?'
+        message: 'Do you expect to use the component in multiple different contexts?',
     },
     {
         type: 'confirm',
         name: 'singleton',
-        message: 'Does the component consist of only a single element?'
+        message: 'Does the component consist of only a single element?',
     },
     {
         type: 'confirm',
         name: 'list',
-        message: 'Does the component contain a list of repeating components?'
+        message: 'Does the component contain a list of repeating components?',
     },
     {
         type: 'confirm',
         name: 'listItem',
-        message: 'Is the component an item in a list of components?'
+        message: 'Is the component an item in a list of components?',
     },
     {
         type: 'confirm',
         name: 'complexity',
-        message: 'Is the component large and complex?'
+        message: 'Is the component large and complex?',
     },
     {
         type: 'confirm',
         name: 'hasAtoms',
-        message: 'Are there any atom components embedded into the component?'
+        message: 'Are there any atom components embedded into the component?',
     },
     {
         type: 'confirm',
         name: 'hasMolecules',
-        message: 'Are there any molecule components embedded into the component?'
+        message: 'Are there any molecule components embedded into the component?',
     },
     {
         type: 'confirm',
         name: 'embedInMolecule',
-        message: 'Is or will the component ever be embedded inside a molecule component?'
+        message: 'Is or will the component ever be embedded inside a molecule component?',
     },
     {
         type: 'confirm',
         name: 'embedInOrganism',
-        message: 'Is or will the component ever be embedded inside an organism component?'
+        message: 'Is or will the component ever be embedded inside an organism component?',
     },
     {
         type: 'list',
         name: 'wrapper',
         message: 'What element is the outer wrapper for the component?',
-        choices: Object.keys(wrapperElements)
-    }
+        choices: Object.keys(wrapperElements),
+    },
 ]
