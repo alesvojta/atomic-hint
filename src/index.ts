477f8261979eac002b0d17cd2e79ef855c32fdf5
Aleš Vojta <al3s.vojta@gmail.com>
2020-05-02 22:42:13 +0200

    refactor: newer prettier code reformat

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import { questions } from './shared/questions'
 import { calculateScore } from './index/calculateScore'
 import { showScore } from './index/showScore'
 
-export default function(): void {
-    prompt(questions)
-        .then(calculateScore)
-        .then(showScore)
-        .catch(console.log)
+export default function (): void {
+    prompt(questions).then(calculateScore).then(showScore).catch(console.log)
 }
--- src/index/calculateScore/scoreReducer.ts
+++ src/index/calculateScore/scoreReducer.ts
@@ -4,6 +4,6 @@ export function scoreReducer(prev: TScore, curr: TScore): TScore {
     return {
         o: prev.o + curr.o,
         m: prev.m + curr.m,
-        a: prev.a + curr.a
+        a: prev.a + curr.a,
     }
 }
